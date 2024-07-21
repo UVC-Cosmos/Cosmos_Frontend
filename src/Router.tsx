4b1fad8f01ccee297570a461bbc3240e171a564e
khakisage <jongjin201@gmail.com>
2024-07-21 13:37:06 +0900

    Remove authentication components and routes
    #1

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router';
-import App from './pages/AuthP';
 import AuthPage from './pages/AuthPage';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
-      <Route path="/auth" element={<App />} />
     </Routes>
   );
 };
