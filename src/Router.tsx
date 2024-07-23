a624875d11b4cddfbe70e35bad54d3fd2a605e4e
khakisage <jongjin201@gmail.com>
2024-07-23 15:48:28 +0900

    feat: Add route for dashboard page in main layout
    This commit adds a new route for the dashboard page in the main layout. The dashboard page will be accessible under the "/main/dashboard" path. This change allows users to navigate to the dashboard page and view relevant information.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import { AdminLayout } from './components/layout/AdminLayout';
+import { DefaultLayout } from './components/layout/DefaultLayout';
 import AuthPage from './pages/AuthPage';
-import MemberListPage from './pages/MemberListPage';
 import DashBoardPage from './pages/DashBoardPage';
+import MemberListPage from './pages/MemberListPage';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
+      <Route path="main" element={<DefaultLayout />}>
+        <Route path="dashboard" element={<DashBoardPage />} />
+      </Route>
       <Route path="admin" element={<AdminLayout />}>
         <Route path="member" element={<MemberListPage />} />
       </Route>
-      <Route path="/dashboard" element={<DashBoardPage />} />
     </Routes>
   );
 };
--- src/pages/DashBoardPage.tsx
+++ src/pages/DashBoardPage.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import UnityComponent from "../components/unity/UnityComponent";
-import useSocket from "../hooks/useSocket";
+import UnityComponent from '../components/unity/UnityComponent';
+import useSocket from '../hooks/useSocket';
 
-const WebSocketServerUrl = import.meta.env.VITE_WEBSOCKET_SERVER_URL;  // 노드서버 Url ex) http://192.168.0.16:3000
+const WebSocketServerUrl = import.meta.env.VITE_WEBSOCKET_SERVER_URL; // 노드서버 Url ex) http://192.168.0.16:3000
 const EdukitId = import.meta.env.VITE_EDUKIT_ID; // edukitId ex) UVC-EDU-01
 
 const DashBoardPage = () => {
@@ -11,31 +10,59 @@ const DashBoardPage = () => {
   return (
     <>
       <UnityComponent />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 20 }}>
+      <div
+        style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 20 }}
+      >
         <div style={{ marginBottom: 10 }}>
-          <button onClick={() => sendMessage("1", "1")} style={buttonStyle}>Start</button>
-          <button onClick={() => sendMessage("1", "0")} style={buttonStyle}>Stop</button>
-          <button onClick={() => sendMessage("8", "1")} style={buttonStyle}>Reset</button>
+          <button onClick={() => sendMessage('1', '1')} style={buttonStyle}>
+            Start
+          </button>
+          <button onClick={() => sendMessage('1', '0')} style={buttonStyle}>
+            Stop
+          </button>
+          <button onClick={() => sendMessage('8', '1')} style={buttonStyle}>
+            Reset
+          </button>
         </div>
         <div style={{ marginBottom: 10 }}>
-          <button onClick={() => sendMessage("9", "1")} style={buttonStyle}>1호기 ON</button>
-          <button onClick={() => sendMessage("9", "0")} style={buttonStyle}>1호기 OFF</button>
+          <button onClick={() => sendMessage('9', '1')} style={buttonStyle}>
+            1호기 ON
+          </button>
+          <button onClick={() => sendMessage('9', '0')} style={buttonStyle}>
+            1호기 OFF
+          </button>
         </div>
         <div style={{ marginBottom: 10 }}>
-          <button onClick={() => sendMessage("10", "1")} style={buttonStyle}>2호기 ON</button>
-          <button onClick={() => sendMessage("10", "0")} style={buttonStyle}>2호기 OFF</button>
+          <button onClick={() => sendMessage('10', '1')} style={buttonStyle}>
+            2호기 ON
+          </button>
+          <button onClick={() => sendMessage('10', '0')} style={buttonStyle}>
+            2호기 OFF
+          </button>
         </div>
         <div style={{ marginBottom: 10 }}>
-          <button onClick={() => sendMessage("11", "1")} style={buttonStyle}>3호기 ON</button>
-          <button onClick={() => sendMessage("11", "0")} style={buttonStyle}>3호기 OFF</button>
+          <button onClick={() => sendMessage('11', '1')} style={buttonStyle}>
+            3호기 ON
+          </button>
+          <button onClick={() => sendMessage('11', '0')} style={buttonStyle}>
+            3호기 OFF
+          </button>
         </div>
         <div style={{ marginBottom: 10 }}>
-          <button onClick={() => sendMessage("12", "1")} style={buttonStyle}>Sensor1 ON</button>
-          <button onClick={() => sendMessage("12", "0")} style={buttonStyle}>Sensor1 OFF</button>
+          <button onClick={() => sendMessage('12', '1')} style={buttonStyle}>
+            Sensor1 ON
+          </button>
+          <button onClick={() => sendMessage('12', '0')} style={buttonStyle}>
+            Sensor1 OFF
+          </button>
         </div>
         <div style={{ marginBottom: 10 }}>
-          <button onClick={() => sendMessage("13", "1")} style={buttonStyle}>Sensor2 ON</button>
-          <button onClick={() => sendMessage("13", "0")} style={buttonStyle}>Sensor2 OFF</button>
+          <button onClick={() => sendMessage('13', '1')} style={buttonStyle}>
+            Sensor2 ON
+          </button>
+          <button onClick={() => sendMessage('13', '0')} style={buttonStyle}>
+            Sensor2 OFF
+          </button>
         </div>
       </div>
     </>
