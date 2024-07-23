567e3df3428f85642478c65dc8c20fd61b74601e
leejhyun <leejh44351@naver.com>
2024-07-23 15:45:04 +0900

    [FEAT] Layout 기본 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import AuthPage from './pages/AuthPage';
 import MemberListPage from './pages/MemberListPage';
+import EditUserPage from './pages/EditUserPage';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
       <Route path="/admin" element={<MemberListPage />} />
+      <Route path="/edit" element={<EditUserPage />} />
     </Routes>
   );
 };
