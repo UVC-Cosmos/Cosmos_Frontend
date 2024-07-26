86a16c70671e6f827afcd2138a4278434dec7a22
khakisage <jongjin201@gmail.com>
2024-07-26 18:30:50 +0900

    feat: Fix role check in AuthRoute component
    The role check in the AuthRoute component was incorrect, causing unauthorized users to be redirected to the wrong page. This commit fixes the role check to redirect unauthorized users to the main page.

--- src/components/route/AuthRotue.tsx
+++ src/components/route/AuthRotue.tsx
@@ -10,7 +10,7 @@ export const AuthRoute = ({ children }: IAuthRouteProps) => {
   const user = JSON.parse(localStorage.getItem('user') || '{}') as IUser;
   const role = user.role;
 
-  if (role === 'User') {
+  if (role !== 'User') {
     // 허용되지 않은 role인 경우 메인 페이지로 리다이렉트
     alert('접근 권한이 없습니다.');
     return <Navigate to="/admin/member" replace />;
