1c68d1aea4064255fb18df983e75ef207c8a51e5
khakisage <jongjin201@gmail.com>
2024-07-26 17:23:49 +0900

    design : 테일윈드 변수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,10 @@ export default {
     },
     colors: {
       mainColor: 'rgba(24, 18, 43, 1)',
+      mainColorM: 'rgba(46,35,108,100)',
       mainColorH: 'rgb(57, 48, 83)',
       mainLightColor: '#635985',
+      dashColor: '#C8ACD6',
       white: '#ffffff',
       black: '#000000'
     }
