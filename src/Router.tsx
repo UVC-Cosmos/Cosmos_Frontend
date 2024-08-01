fbd96061e127a3b2125f9a97d64789a2cc120c04
khakisage <jongjin201@gmail.com>
2024-08-01 09:55:18 +0900

    refactor : 로그인 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,7 @@
-import { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { AdminLayout } from './components/layout/AdminLayout';
 import { DefaultLayout } from './components/layout/DefaultLayout';
 import { ManagerLayout } from './components/layout/ManagerLayout';
-import { IUser } from './interface/authInterface';
 import AuthPage from './pages/AuthPage';
 import DashBoardPage from './pages/DashBoardPage';
 import EditUserPage from './pages/EditUserPage';
@@ -12,30 +10,6 @@ import MemberListPage from './pages/MemberListPage';
 import { PermissionPage } from './pages/PermissionPage';
 
 const Router = () => {
-  const navigate = useNavigate();
-  const isLogin = localStorage.getItem('isLogin') === 'true';
-  const user: IUser = JSON.parse(localStorage.getItem('user') || '{}');
-  const userType = user.role;
-
-  useEffect(() => {
-    if (isLogin) {
-      switch (userType) {
-        case 'User':
-          navigate('/main/dashboard');
-          break;
-        case 'Admin':
-          navigate('/admin/member');
-          break;
-        case 'A-factoryAdmin':
-        case 'B-factoryAdmin':
-        case 'C-factoryAdmin':
-          navigate('/factory/member');
-          break;
-        default:
-          break;
-      }
-    }
-  }, [isLogin, navigate, userType]);
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
